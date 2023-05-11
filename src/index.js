b9ab2bef76dc44dca8c329d9171a03229c5024ac
Jason Ritter <jason_ritter@outlook.com>
2023-05-11 17:07:11 -0500

    Push finished project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import foods from './foods';
+import {choice, remove} from './helpers';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const decision = choice(foods);
+const foodsLeft = remove(foods, decision);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>I'd like one {decision}, please.</p>
+                <p>Here you go: {decision}</p>
+                <p>Delicious! May I have another?"</p>
+                <p>I'm sorry, we're all out. We have {foodsLeft.length} ({foodsLeft}) left.</p>
+                <p></p>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
